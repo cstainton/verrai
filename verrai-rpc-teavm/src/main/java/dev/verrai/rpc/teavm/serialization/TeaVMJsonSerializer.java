fa27e068d6f93f13b128160f790ceda4054f5b55
cstainton <4094997+cstainton@users.noreply.github.com>
2026-02-22 20:59:04 +0000

    Remove unused import in TeaVMJsonSerializer

--- verrai-rpc-teavm/src/main/java/dev/verrai/rpc/teavm/serialization/TeaVMJsonSerializer.java
+++ verrai-rpc-teavm/src/main/java/dev/verrai/rpc/teavm/serialization/TeaVMJsonSerializer.java
@@ -1,7 +1,5 @@
 package dev.verrai.rpc.teavm.serialization;
 
-import dev.verrai.rpc.common.serialization.Serializer;
-
 public class TeaVMJsonSerializer extends dev.verrai.rpc.common.serialization.ReflectionFreeJsonSerializer {
     // Inherits implementation from ReflectionFreeJsonSerializer which uses
     // JsonCodecRegistry
